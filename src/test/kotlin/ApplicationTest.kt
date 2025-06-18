0309ed9a50670a1998c74eae9a12a8f729c3fa15
JoseDA0721 <133171655+JoseDA0721@users.noreply.github.com>
2025-06-18 02:56:03 -0500

    Update test

--- src/test/kotlin/ApplicationTest.kt
+++ src/test/kotlin/ApplicationTest.kt
@@ -2,7 +2,7 @@ package com.example
 
 import io.ktor.client.request.get
 import io.ktor.http.HttpStatusCode
-import io.ktor.server.config.MapApplicationConfig
+import io.ktor.server.config.ApplicationConfig
 import io.ktor.server.testing.testApplication
 import kotlin.test.Test
 import kotlin.test.assertEquals
@@ -12,13 +12,7 @@ class ApplicationTest {
     fun testRoot() =
         testApplication {
             environment {
-                config =
-                    MapApplicationConfig(
-                        "database.driver" to "org.postgresql.Driver",
-                        "database.url" to "jdbc:postgresql://localhost:5432/testdb",
-                        "database.user" to "testuser",
-                        "database.password" to "testpassword",
-                    )
+                config = ApplicationConfig("application-tets.yaml")
             }
 
             client.get("/").apply {
